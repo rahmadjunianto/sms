84fd47c8a5773417f4b929a8038c2a0e58456f61
rahmadjunianto <rahmadjunianto.rj@gmail.com>
2018-01-19 16:09:37 +0700

    sinkronisasi harga

--- application/modules/Gudang/models/Mtr_barang_masuk.php
+++ application/modules/Gudang/models/Mtr_barang_masuk.php
@@ -23,8 +23,18 @@ class Mtr_barang_masuk extends CI_Model
         return $this->datatables->generate();
     }
     function json_sukses() {
+        $ku=$this->session->userdata('ku');
         $this->datatables->select("no_faktur,kd_barang_masuk,nm_barang,DATE_FORMAT(tanggal, '%d-%m-%Y') as tanggal,harga,jumlah");
         $this->datatables->from('tr_barang_masuk_log ');;$this->db->order_by("tanggal", "desc");
+        $this->datatables->where("status='sukses' and kd_pengguna=$ku");
+        $this->datatables->add_column('action', '<div class="btn-group">'.anchor(site_url('gudang/tr_barang_masuk/update/$1'),'<i class="fa fa-edit"></i>','class="btn btn-xs btn-success"').anchor(site_url('gudang/tr_barang_masuk/delete/$1'),'<i class="fa fa-trash"></i>','class="btn btn-xs btn-danger" onclick="javasciprt: return confirm(\'Apakah anda yakin?\')"').'</div>', 'kd_barang_masuk');
+        return $this->datatables->generate();
+    }
+    function json_gagal() {
+        $ku=$this->session->userdata('ku');
+        $this->datatables->select("no_faktur,kd_barang_masuk,nm_barang,DATE_FORMAT(tanggal, '%d-%m-%Y') as tanggal,harga,jumlah");
+        $this->datatables->from('tr_barang_masuk_log ');;$this->db->order_by("tanggal", "desc");
+        $this->datatables->where("status='gagal' and kd_pengguna=$ku");
         $this->datatables->add_column('action', '<div class="btn-group">'.anchor(site_url('gudang/tr_barang_masuk/update/$1'),'<i class="fa fa-edit"></i>','class="btn btn-xs btn-success"').anchor(site_url('gudang/tr_barang_masuk/delete/$1'),'<i class="fa fa-trash"></i>','class="btn btn-xs btn-danger" onclick="javasciprt: return confirm(\'Apakah anda yakin?\')"').'</div>', 'kd_barang_masuk');
         return $this->datatables->generate();
     }
--- application/modules/Gudang/views/tr_barang_masuk/data_import.php
+++ application/modules/Gudang/views/tr_barang_masuk/data_import.php
@@ -32,6 +32,35 @@
                   </div>
                 </div>
               </div>
+            </div><?php }  ?><?php if( $this->session->userdata('gagal')!=0){?>
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel"><?php echo $this->session->flashdata('notif');?> 
+                  <div class="x_title">
+            <h2>
+              Data Barang Masuk Gagal Import
+
+            </h2>                            
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                    <table id="example2" class="table table-striped table-bordered">
+                      <thead>
+                        <tr>
+                          <th width="5%">No</th>
+                          <th width="15%">Tanggal</th>
+                          <th width="15%">No Faktur</th>
+                          <th width="15%">Nama barang</th>
+                          <th width="15%">Jumlah</th>
+                        </tr>
+                      </thead>
+
+
+                      
+                    </table>
+                  </div>
+                </div>
+              </div>
             </div><?php }  ?>
        <script type="text/javascript">
             $(document).ready(function() {
@@ -83,6 +112,73 @@
                     processing: true,
                     serverSide: true,
                     ajax: {"url": "<?php echo base_url()?>gudang/tr_barang_masuk/jsonsukses", "type": "POST"},
+                    columns: [
+                        {
+                            "data": "kd_barang_masuk",
+                            "orderable": false,
+                            "className" : "text-center",
+                        },{"data": "tanggal"},{"data": "no_faktur"},{"data": "nm_barang"},{"data": "jumlah"}
+                    ],
+                    rowCallback: function(row, data, iDisplayIndex) {
+                        var info = this.fnPagingInfo();
+                        var page = info.iPage;
+                        var length = info.iLength;
+                        var index = page * length + (iDisplayIndex + 1);
+                        $('td:eq(0)', row).html(index);
+                    }
+                });
+            });
+        </script>
+       <script type="text/javascript">
+            $(document).ready(function() {
+                $.fn.dataTableExt.oApi.fnPagingInfo = function(oSettings)
+                {
+                    return {
+                        "iStart": oSettings._iDisplayStart,
+                        "iEnd": oSettings.fnDisplayEnd(),
+                        "iLength": oSettings._iDisplayLength,
+                        "iTotal": oSettings.fnRecordsTotal(),
+                        "iFilteredTotal": oSettings.fnRecordsDisplay(),
+                        "iPage": Math.ceil(oSettings._iDisplayStart / oSettings._iDisplayLength),
+                        "iTotalPages": Math.ceil(oSettings.fnRecordsDisplay() / oSettings._iDisplayLength)
+                    };
+                };
+
+                var t = $("#example2").dataTable({
+                    initComplete: function() {
+                        var api = this.api();
+                        $('#mytable_filter input')
+                                .off('.DT')
+                                .on('keyup.DT', function(e) {
+                                    if (e.keyCode == 13) {
+                                        api.search(this.value).draw();
+                            }
+                        });
+                    },
+                    
+                        
+                    
+                    'oLanguage':
+                    {
+                      "sProcessing":   "Sedang memproses...",
+                      "sLengthMenu":   "Tampilkan _MENU_ entri",
+                      "sZeroRecords":  "Tidak ditemukan data yang sesuai",
+                      "sInfo":         "Menampilkan _START_ sampai _END_ dari _TOTAL_ entri",
+                      "sInfoEmpty":    "Menampilkan 0 sampai 0 dari 0 entri",
+                      "sInfoFiltered": "(disaring dari _MAX_ entri keseluruhan)",
+                      "sInfoPostFix":  "",
+                      "sSearch":       "Cari:",
+                      "sUrl":          "",
+                      "oPaginate": {
+                        "sFirst":    "Pertama",
+                        "sPrevious": "Sebelumnya",
+                        "sNext":     "Selanjutnya",
+                        "sLast":     "Terakhir"
+                      }
+                    },
+                    processing: true,
+                    serverSide: true,
+                    ajax: {"url": "<?php echo base_url()?>gudang/tr_barang_masuk/jsongagal", "type": "POST"},
                     columns: [
                         {
                             "data": "kd_barang_masuk",
