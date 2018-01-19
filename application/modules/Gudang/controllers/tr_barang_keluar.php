ddb2bb1de94bb5ce6ca65aa822e5cc1e7fe8e02b
rahmadjunianto <rahmadjunianto.rj@gmail.com>
2018-01-19 17:08:35 +0700

    fix import barang keluar

--- application/modules/Gudang/controllers/tr_barang_keluar.php
+++ application/modules/Gudang/controllers/tr_barang_keluar.php
@@ -74,7 +74,16 @@ class tr_barang_keluar extends CI_Controller {
             $ku=$this->session->userdata('ku');
             $temp = $this->db->query("select * from tr_barang_keluar_importtemp WHERE kd_pengguna=$ku")->result();
             foreach ($temp as $temp) {
-
+        $jumlah_keluar   = $temp->jumlah;
+        $row=$this->db->query("SELECT * FROM ref_barang WHERE kd_barang=$temp->kd_barang")->row();
+        if($row)
+        {
+            $stock=$row->stock;
+            $stock_baru=$stock-$jumlah_keluar;
+        }
+        $update = array('stock'=>$stock_baru );
+        $this->db->where('kd_barang', $temp->kd_barang);
+        $this->db->update('ref_barang', $update); 
                     $this->db->query("INSERT INTO tr_barang_keluar (kd_barang,tanggal,jumlah,harga,nm_barang,kd_unit) VALUES ('$temp->kd_barang','$temp->tanggal','$temp->jumlah','$temp->harga','$temp->nm_barang','$temp->kd_unit')");
                     $this->db->query("INSERT INTO tr_barang_keluar_log (kd_barang,tanggal,jumlah,harga,nm_barang,status,kd_pengguna,kd_unit) VALUES ('$temp->kd_barang','$temp->tanggal','$temp->jumlah','$temp->harga','$temp->nm_barang','sukses','$ku','$temp->kd_unit')");
                         $berhasil++;
@@ -113,6 +122,8 @@ class tr_barang_keluar extends CI_Controller {
     }
     public function create_action() 
     {
+        $kd_barang = $this->input->post('barang',TRUE);
+        $jumlah_keluar    = $this->input->post('jumlah',TRUE);
         $data = array(
 		'kd_barang' => $this->input->post('barang',TRUE),
 		'tanggal' => substr($this->input->post('date',TRUE),6,4)."-".substr($this->input->post('date',TRUE),3,2)."-".substr($this->input->post('date',TRUE),0,2),
@@ -121,7 +132,15 @@ class tr_barang_keluar extends CI_Controller {
         'nm_barang' => $this->input->post('nm_barang',TRUE),
         'kd_unit' => $this->input->post('kd_unit',TRUE),
 	    );
-
+        $row=$this->db->query("SELECT * FROM ref_barang WHERE kd_barang=$kd_barang")->row();
+        if($row)
+        {
+            $stock=$row->stock;
+            $stock_baru=$stock-$jumlah_keluar;
+        }
+        $update = array('stock'=>$stock_baru );
+        $this->db->where('kd_barang', $kd_barang);
+        $this->db->update('ref_barang', $update);
             $this->Mtr_barang_keluar->insert($data);
             $this->session->set_flashdata('message', '<button type="button" class="btn btn-success"> Berhasil Menambah barang Keluar</button>');
             redirect(site_url('gudang/tr_barang_keluar'));
--- application/modules/Gudang/views/tr_barang_keluar/import_form.php
+++ application/modules/Gudang/views/tr_barang_keluar/import_form.php
@@ -8,7 +8,7 @@
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Import Form</h2>                   
+                    <h2>Import Form Barang Keluar</h2>                   
                     <div class="clearfix"></div>
                   </div>
                   <div class="x_content">
